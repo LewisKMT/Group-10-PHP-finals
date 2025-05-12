276c51a4b612fdf75cf954cfb0daa67c6db9a393
Lew <lewiskieltierra@gmail.com>
2025-05-12 16:57:45 +0800

    Update index.php

--- htdocs/signup-login/index.php
+++ htdocs/signup-login/index.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h2>Welcome</h2>
+    <form action="signup.php" method="get" style="display:inline;">
+        <button type="submit">Sign Up</button>
+    </form>
+    <form action="login.php" method="get" style="display:inline;">
+        <button type="submit">Log In</button>
+    </form>
+</body>
+</html>
