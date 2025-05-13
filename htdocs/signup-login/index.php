b36521ce9ca77c81592b31b2823f266d68db50ac
LewisKMT <lewiskieltierra@gmail.com>
2025-05-13 21:09:05 +0800

    test

--- htdocs/signup-login/index.php
+++ htdocs/signup-login/index.php
@@ -6,7 +6,6 @@
 <body>
 
 <?php
-// Test
 // Show success message after signup
 if (isset($_GET['signup']) && $_GET['signup'] === 'success') {
     echo "<p style='color: green;'>Account created successfully. You can now log in.</p>";
