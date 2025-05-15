651f3c167d1302e4a868a2791e9b2d7775964939
Van Russel <vanrusselstaana@gmail.com>
2025-05-15 19:26:33 +0800

    tnc content

--- htdocs/signup-login/tnc.php
+++ htdocs/signup-login/tnc.php
@@ -21,113 +21,71 @@
 				display: flex;
 				justify-content: center;
 				align-items: center;
-				background-color: yellow;
+				background-color: #993300;
 				padding: 4rem;
 			}
 
 			.container {
 				height: 90vh;
-				background-color: red;
-				color: blue;
+				width: 40rem;
+				background-color: #ffcc66;
+				color: #333333;
+				box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
 				padding: 2rem;
 				border: none;
 				border-radius: 40px;
 				overflow-y: auto;
+			}
+			h1 {
+				color: #2c3e50;
+			}
+			h2 {
+				margin-top: 30px;
+			}
+			p {
+				line-height: 1.6;
 			}
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<h1>Terms and Conditions</h1>
-			<br />
+			<p><strong>Effective Date:</strong> [Insert Date]</p>
+
+			<h2>1. Use of Our Service</h2>
 			<p>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
-				voluptatum at laborum et magnam ipsam molestias, dolorum culpa tempore
-				quo sit eum aliquam fugiat! Ipsam similique, corrupti laboriosam
-				exercitationem magnam sapiente ut nisi quas eius enim optio inventore
-				earum ullam molestias facere veniam asperiores? Nisi culpa iure, maiores
-				iusto odio maxime voluptate ipsam temporibus, quod asperiores doloribus
-				odit. Placeat similique vero corporis maxime officiis quis nulla in
-				animi incidunt reiciendis, cumque dolorem, ea libero assumenda ipsum?
-				Consequatur, soluta incidunt cupiditate nam quaerat vero molestiae
-				nesciunt nostrum, tempora vel aliquid, repellat autem at officiis
-				doloribus eius neque perspiciatis iure? Porro, alias qui dicta ratione
-				ab minima ut neque doloribus a optio soluta modi esse nostrum in illo
-				assumenda sit architecto at est laudantium sequi harum nemo libero.
-				Repudiandae praesentium consequuntur doloremque reiciendis tempore ullam
-				laudantium, voluptas iure voluptate quam, sed adipisci odit fuga, vel
-				eligendi reprehenderit blanditiis animi facilis id illum nisi atque
-				dolorum dolor! Consequuntur molestiae illum laudantium cumque voluptates
-				at, alias tempore hic iste veritatis blanditiis aliquam sunt repudiandae
-				sint optio unde, eius cum expedita, earum tenetur vitae aliquid
-				quibusdam quaerat odit. Provident, fuga praesentium saepe aperiam
-				tempore repudiandae laborum libero deserunt recusandae dolores sit
-				aliquam velit! Voluptatum inventore consequuntur, id repudiandae nam a
-				animi consectetur, amet ducimus ea, voluptatibus sequi aliquid alias
-				possimus labore laudantium harum. Nisi tenetur omnis temporibus error
-				voluptatum fugit sed qui non ut ratione, et aut architecto incidunt
-				quas, magnam rem! Placeat velit minus quibusdam omnis! Illo ipsa culpa
-				necessitatibus voluptatem, quae nulla mollitia dolor accusantium sunt
-				natus error quaerat perspiciatis et? Tempore explicabo minus
-				praesentium, eos cumque fugit fugiat facere sed nostrum, consequatur
-				sapiente ullam laudantium quisquam minima, consequuntur libero corporis
-				quae! Quod, placeat! Excepturi, optio sapiente, sit voluptatibus, est
-				deleniti porro esse inventore hic at cumque consectetur facilis
-				accusantium modi nemo ea nihil quasi? Ex odit optio recusandae officiis!
-				Officia laboriosam ullam architecto ratione voluptatibus consequatur
-				suscipit natus dolor modi qui voluptas quam similique labore, commodi
-				rerum ipsam autem aperiam praesentium, et doloremque incidunt eligendi
-				delectus nemo? Facere fugiat, officiis aspernatur facilis exercitationem
-				praesentium cum quod, aperiam ut aliquid laboriosam laudantium excepturi
-				modi tenetur perspiciatis voluptas repellendus! Eos pariatur quam, ullam
-				exercitationem architecto minus fugit nihil enim nemo, ex, delectus
-				beatae recusandae dolore hic tenetur culpa voluptatem maxime non cum sed
-				atque fuga? Quo, fugit. Fuga repellat vel necessitatibus? Qui dolores
-				incidunt aliquam explicabo provident accusamus ipsum consequuntur est ad
-				tenetur saepe vero sit fugiat, corporis debitis doloremque, voluptatum
-				accusantium corrupti repellat maiores! Corrupti id autem, et assumenda
-				tempore eligendi natus pariatur alias reiciendis impedit dolor ipsam
-				similique reprehenderit ea consequatur illo delectus voluptatem
-				quibusdam quos omnis tenetur earum! Amet libero impedit aliquam placeat,
-				qui voluptatem earum, quae illo dolorum possimus facilis dolorem, ut
-				aliquid. Quae laudantium iusto deserunt nobis vel sunt consectetur
-				itaque beatae animi cumque nisi ratione sit quo officia ullam similique
-				recusandae fuga, voluptates molestias repudiandae nam debitis reiciendis
-				neque accusantium. Ipsum nulla quo totam tenetur est porro repellendus
-				rem necessitatibus laudantium tempore vitae, molestiae inventore nisi
-				qui autem sit quasi ab odit non! Repellendus voluptas exercitationem
-				iste, cum accusamus velit ad iusto et error ab consectetur incidunt quae
-				atque esse ut maxime amet dolore fugit. Distinctio, laborum ad! Atque
-				quo dicta ad ipsam nesciunt assumenda rerum. Natus in, dicta ducimus
-				accusamus error porro similique ratione distinctio, nemo corrupti ut
-				quis. Nisi a fugiat qui et quasi voluptate, amet itaque maxime vel natus
-				maiores quis quibusdam doloribus magni corporis facilis autem?
-				Reprehenderit ut vero culpa ipsum molestias eum laborum? Voluptatum,
-				esse perferendis error laborum cum eum ad rem veniam, ex beatae facilis
-				alias magni cupiditate quisquam sed, impedit harum hic praesentium.
-				Dolorem tempora consequatur non, quam aliquid quo obcaecati consequuntur
-				similique. Excepturi expedita sint dicta earum tempora quisquam minus,
-				quaerat officia? Impedit voluptate temporibus laborum dolorum explicabo
-				dignissimos eveniet quas, suscipit officiis dicta nobis ea, iusto quo
-				alias, possimus nisi assumenda hic. Maiores consequatur facilis, magni,
-				temporibus doloremque praesentium iusto debitis doloribus blanditiis
-				commodi recusandae? Praesentium id, quidem modi illum totam adipisci,
-				iusto odit fugit perferendis aperiam, deserunt ipsum voluptatem maxime
-				corrupti repellendus quia? Sit veritatis reprehenderit natus explicabo
-				voluptas nobis expedita, adipisci praesentium, qui tenetur omnis
-				necessitatibus architecto! Perspiciatis cum temporibus tempora aperiam
-				similique cumque optio libero alias quam et quo quia culpa cupiditate
-				dignissimos esse aut itaque enim nisi ipsam aliquam explicabo, qui,
-				voluptatibus sunt necessitatibus? Quaerat pariatur corrupti voluptatum
-				sunt quia veniam deleniti a maiores cumque vitae. Quo fugiat dolor
-				recusandae fuga accusantium odio rem ducimus voluptatem atque
-				repellendus neque repellat, expedita, vel rerum harum accusamus veniam
-				nostrum id ipsa. Accusantium sed molestias quo quos architecto repellat,
-				dignissimos aspernatur atque officia sapiente necessitatibus amet, hic
-				at placeat ipsa repellendus officiis nisi libero est nihil vitae? Vitae
-				eveniet quaerat ipsam animi. Inventore earum dolores et eum quo natus
-				tenetur quisquam beatae ullam eveniet. Est quaerat inventore mollitia.
+				By using our service, you agree to act responsibly and follow all laws.
+				Don’t misuse or damage the service.
 			</p>
+
+			<h2>2. Accounts</h2>
+			<p>
+				If you create an account, keep your login safe. You’re responsible for
+				everything done under your account.
+			</p>
+
+			<h2>3. Content</h2>
+			<p>
+				Don’t upload or post anything illegal or harmful. We can remove content
+				that breaks the rules.
+			</p>
+
+			<h2>4. Changes to the Service</h2>
+			<p>We might update or change the service without notice.</p>
+
+			<h2>5. Ending Our Service</h2>
+			<p>
+				We can stop the service or suspend your access if you break these terms.
+			</p>
+
+			<h2>6. Liability</h2>
+			<p>
+				We try to keep everything working, but we’re not responsible for any
+				loss or damage caused by using our service.
+			</p>
+
+			<h2>7. Changes to These Terms</h2>
+			<p>These terms might change. We’ll notify you of important updates.</p>
 		</div>
 	</body>
 </html>
