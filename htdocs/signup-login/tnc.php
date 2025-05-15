63e9acbbc379cbc7258140769b0934990a1c3ccc
Van Russel <vanrusselstaana@gmail.com>
2025-05-15 14:13:28 +0800

    signup bg

---
+++ htdocs/signup-login/tnc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Terms and Conditions</title>
+  <style>
+    
+  </style>
+</head>
+<body>
+  <div class="container">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, ab aliquid. Illum perferendis, natus non error magni nam qui cum necessitatibus ipsum voluptatem ab sunt laboriosam voluptas ad asperiores nemo ullam odit. Quos itaque qui eum nisi ratione quis deserunt sunt excepturi inventore laboriosam tempore natus fugiat, exercitationem, unde atque debitis voluptatibus. Doloribus, deleniti nesciunt in libero voluptate accusantium sunt quaerat quos enim distinctio itaque corporis tempore officiis quod labore nisi sapiente. Earum sit pariatur eos voluptate nemo nisi quaerat vitae necessitatibus libero accusamus numquam assumenda id dolorem, commodi cupiditate ex consequuntur obcaecati beatae fugit repellendus distinctio nihil modi. Nobis quos, modi dolor possimus quasi doloremque eaque architecto corporis suscipit! Quas deleniti aliquam provident iusto adipisci in, neque sit, ipsum explicabo nobis sapiente laudantium accusantium eaque hic. Sint accusamus ut minima, porro nostrum asperiores incidunt alias veniam numquam. Distinctio aspernatur maiores molestiae eligendi eum facere maxime amet asperiores rerum dolorum illum fugit aperiam qui sit blanditiis quo nesciunt placeat cumque impedit dignissimos similique quisquam incidunt, dicta fuga? Distinctio nihil accusantium, obcaecati expedita nulla dolor fuga esse quod incidunt ratione porro animi at. Aliquid dignissimos, harum animi molestiae nemo sint cumque fuga. Odio porro et animi! Unde excepturi illo deserunt quasi.
+  </div>
+</body>
+</html>
